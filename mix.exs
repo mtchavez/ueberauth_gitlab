962130ea2b11c45076606ea559cc6bdf117353a8
Chavez <matthew@el-chavez.me>
2017-11-06 08:44:25 -0800

    Add LICENSE to mix

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule UeberauthGitlab.Mixfile do
 
   defp package do
     [
-      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
       maintainers: ["Chavez"],
       licenses: ["MIT"],
       links: %{"GitHub": "https://github.com/mtchavez/ueberauth_gitlab"}
